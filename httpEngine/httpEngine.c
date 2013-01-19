09bd2f728960eb4ff7174aab02d367e39571375e
Ryan Moore <ryan.moore@greyrocksoft.com>
2013-01-19 11:50:43 -0700

    Removed duplication url parsing code in the http engine.

--- httpEngine/httpEngine.c
+++ httpEngine/httpEngine.c
@@ -2,26 +2,11 @@
 #include <string.h>
 #include "httpEngine.h"
 
-
-bool gr_http_createURI(gr_uri *uri_struct, char *uri, size_t length){
-  int index = 0,startIndex,endIndex;
-  //clear out the uri struct
-  memset(uri_struct,0,sizeof(gr_uri));
-  //start reading the characters from the input stream.
-  //find the first non whitespace charachter.
-  while(index < length && isspace(uri[index])){++index;}
-  startIndex = index; // need this to copy the full url at the end.
-  
-
-
-}
-
 void gr_http_start(gr_request *new_request, net_socket socket){
   //clear out the request struct
   memset(new_request,0,sizeof(gr_request));
   
 }
-
 
 void gr_http_error(gr_request *request, int http_error)
 {
